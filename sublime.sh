b800eff58b6552d0c84aac2089f9c481fee89a64
cdyfng <xiaofeng_chen2007@163.com>
2020-05-19 21:13:00 +0800

    sublime: go and solidity syntax

---
+++ sublime.sh
@@ -0,0 +1,10 @@
+#Go lang
+git clone https://github.com/DisposaBoy/GoSublime.git
+mkdir ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+mv GoSublime  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+
+#Solidity
+git clone https://github.com/davidhq/SublimeEthereum
+cp SublimeEthereum/Solidity.sublime-syntax ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+
+
